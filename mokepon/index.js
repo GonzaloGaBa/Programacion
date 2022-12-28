161fdf4414110aa32668060900b006ccdcc8fa42
Gonzalo Barrientos <gongabar@gmail.com>
2022-12-28 16:44:41 -0300

    Segundo commit antes de cagarla

--- mokepon/index.js
+++ mokepon/index.js
@@ -1,1 +1,24 @@
-console.log ("Hola Node")
+const express = require('express');
+
+const app = express();
+
+const port = 8080;
+
+const jugadores = [];
+
+class Jugador {}
+
+app.get('/unirse', (req, res) => {
+    const id = `${Math.random()}`;
+
+    const jugador = new Jugador(id)
+
+    jugadores.push(jugador)
+
+    res.send(id);
+});
+
+
+app.listen(port, () => {
+  console.log(`¡Servidor listo! ${port}`);
+});
